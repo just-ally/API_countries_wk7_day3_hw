501cf4aa25dc49f5d6509f30735d02e9ece85d42
Ally Conway <avconway@hotmail.co.uk>
2019-01-09 23:36:39 +0000

    Adds css and changes flag image to the background image rather than appended to div

--- src/views/country_info_view.js
+++ src/views/country_info_view.js
@@ -13,29 +13,34 @@ CountryInfoView.prototype.bindEvents = function(){
 CountryInfoView.prototype.render = function(country){
   this.container.innerHTML = '';
 
-  const countryName = document.createElement('h3');
-  countryName.textContent = country.name;
+  const countryName = this.addElement('h2', `Country: ${country.name}`);
   this.container.appendChild(countryName);
 
-  const countryRegion = document.createElement('p');
-  countryRegion.textContent = country.region;
+  const countryRegion = this.addElement('p', `Region: ${country.region}`);
   this.container.appendChild(countryRegion);
 
-  const countryFlag = document.createElement('img');
-  countryFlag.classList.add('flag-image');
-  countryFlag.src = country.flag;
-  this.container.appendChild(countryFlag);
+  const languageListHeading = this.addElement('p', `In ${country.name}, the following languages are spoken:`);
+  this.container.appendChild(languageListHeading);
 
-  console.log('Country languages:', country.languages.name);
   const countryLanguages = document.createElement('ul');
   country.languages.forEach((language) => {
-    const listItem = document.createElement('li');
-    listItem.textContent = language.name;
+    const listItem = this.addElement('li', language.name);
     countryLanguages.appendChild(listItem);
   });
   this.container.appendChild(countryLanguages);
 
+  const imageLink = country.flag;
+  document.body.style.backgroundImage = `url(${imageLink})`
+  const countryFlag = document.createElement('img');
+  countryFlag.classList.add('flag-image');
+  countryFlag.src = imageLink;
 
+}
+
+CountryInfoView.prototype.addElement = function(type, text){
+  const element = document.createElement(type);
+  element.textContent = text;
+  return element;
 }
 
 module.exports = CountryInfoView;
