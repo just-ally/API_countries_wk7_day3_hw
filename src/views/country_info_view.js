d7fef4ebfd7126495dd61f19d060f93c06184508
Ally Conway <avconway@hotmail.co.uk>
2019-01-09 17:29:52 +0000

    Completes MVP

--- src/views/country_info_view.js
+++ src/views/country_info_view.js
@@ -13,10 +13,18 @@ CountryInfoView.prototype.bindEvents = function(){
 CountryInfoView.prototype.render = function(country){
   this.container.innerHTML = '';
 
-  const countryName = document.createElement('h2');
+  const countryName = document.createElement('h3');
   countryName.textContent = country.name;
-
   this.container.appendChild(countryName);
+
+  const countryRegion = document.createElement('p');
+  countryRegion.textContent = country.region;
+  this.container.appendChild(countryRegion);
+
+  const countryFlag = document.createElement('img');
+  countryFlag.classList.add('flag-image');
+  countryFlag.src = country.flag;
+  this.container.appendChild(countryFlag);
 }
 
 module.exports = CountryInfoView;
